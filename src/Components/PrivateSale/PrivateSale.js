ca3b26a0cab250b7c0a84bbb042f4af3e77074d9
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-05-24 08:30:48 +0600

    Section1 worked

---
+++ src/Components/PrivateSale/PrivateSale.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./PrivateSale.css";
+import Countdown from "react-countdown";
+
+// Random component
+const Completionist = () => <span>You are good to go!</span>;
+
+// Renderer callback with condition
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a completed state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <div className="counter justify-content-center  d-flex mt-2 text-center align-items-center">
+        <div className="counter_item mx-4">
+          <span className="time">{days}</span>
+          <br />
+          <span className="time_name">Days</span>
+        </div>{" "}
+        :
+        <div className="counter_item mx-4">
+          <span className="time">{hours}</span>
+          <br />
+
+          <span className="time_name">Hours</span>
+        </div>{" "}
+        :
+        <div className="counter_item mx-4">
+          <span className="time">{minutes}</span>
+          <br />
+          <span className="time_name">Minutes</span>
+        </div>{" "}
+        :
+        <div className="counter_item mx-4">
+          <span className="time">{seconds}</span>
+          <br />
+          <span className="time_name">Seconds</span>
+        </div>
+      </div>
+    );
+  }
+};
+
+function PrivateSale(props) {
+  return (
+    <div id="PrivateSale">
+      <h2>
+        Private <br /> Sale
+      </h2>
+      <div className="private_sale_counter text-center">
+        <Countdown date={Date.now() + 1036800000} renderer={renderer} />
+      </div>
+      <div className="counter_input_wrapper w-75 mx-auto">
+        <input type="text"  defaultValue={1300} />
+        <button>MAX</button>
+      </div>
+
+      <div className="helper">
+        <small className="d-block p-2">1 BUSD = 500 $ROYALE</small>
+
+        <br />
+        <button className="btn_frame">Buy with BUSD</button>
+      </div>
+    </div>
+  );
+}
+
+export default PrivateSale;
---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Homepage.css";
+import PrivateSale from "../../Components/PrivateSale/PrivateSale";
+function Homepage(props) {
+  return (
+    <div id="Homepage">
+      <Section1></Section1>
+    </div>
+  );
+}
+
+export default Homepage;
+
+const Section1 = () => {
+  return <div id="Section1">
+    <div className="section_wrapper">
+      <div className="row">
+        <div className="col-md-7">
+          <h1 className="mb-3 mb-md-5">
+            BIT COMP
+          </h1>
+          <h4 className="mb-3 mb-md-5">
+            BitComp unites investors, players and developers  based
+            on blockchain technologies. <br /><br />
+
+            You can play with your friends, invest in new projects and even create your own token and find investors for it.
+          </h4>
+          <div className="d-flex flex-wrap align-items-center justify-content-center  ">
+            <a href="#" className="me-3 me-md-5">
+              <img src={require('../../Static/img/google_play.png')} alt="img" />
+            </a>
+            <a href="#">
+              <img src={require('../../Static/img/app_store.png')} alt="img" />
+
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <PrivateSale></PrivateSale>
+    </div>
+  </div>
+};        
