ebb22d3e6e61529f75b687b1b4b5051797803095
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-05-25 14:41:20 +0600

    buy nft investory updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import Base from "./Layout/Base/Base";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// ...................Pages...................
 import Homepage from "./Pages/Homepage/Homepage";
+import BuyNftInvestory from "./Pages/BuyNftInvestory/BuyNftInvestory";
+// ...................Pages...................End
+
 function App() {
   return (
     <div className="App">
-      <Base>
-        <Homepage></Homepage>
-      </Base>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <Base> <Homepage></Homepage></Base>
+          }></Route>
+          <Route path="/buy_nft_investory" element={<BuyNftInvestory></BuyNftInvestory>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
---
+++ src/Pages/BuyNftInvestory/BuyNftInvestory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './BuyNftInvestory.css';
+function BuyNftInvestory(props) {
+    return (
+        <div id='BuyNftInvestory'>
+
+        </div>
+    );
+}
+
+export default BuyNftInvestory;
